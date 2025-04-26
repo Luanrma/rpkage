f83b484ae650b1985fd7dd949ab6d41aed5c55a9
Luanrma <luanrma@gmail.com>
2025-04-26 17:14:27 -0300

    start development branch

--- src/app/components/Aside/index.tsx
+++ src/app/components/Aside/index.tsx
@@ -1,6 +1,7 @@
-import { useState, useRef } from "react";
+import { useState, useRef, RefObject } from "react";
 import styled from "styled-components";
 import { Activity, Swords, SquareArrowOutDownLeft, SquareArrowOutUpRight, Backpack } from "lucide-react";
+import dragAndDrop from "@/app/utils/dragAndDrop";
 
 const AsideContainer = styled.div<{ $collapsed: boolean }>`
   position: absolute;
@@ -63,59 +64,12 @@ export default function Aside({ changeSection }: AsideProps) {
 
 	const handleMenuItemClick = (value: string) => changeSection(value)
 
-	const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
-		e.preventDefault();
-
-		const aside = asideRef.current;
-		if (!aside) return;
-
-		const isTouchEvent = 'touches' in e;
-		const startX = isTouchEvent ? e.touches[0].clientX : e.clientX;
-		const startY = isTouchEvent ? e.touches[0].clientY : e.clientY;
-
-		const rect = aside.getBoundingClientRect();
-		const shiftX = startX - rect.left;
-		const shiftY = startY - rect.top;
-
-		const onMove = (moveEvent: MouseEvent | TouchEvent) => {
-			const moveX = 'touches' in moveEvent ? moveEvent.touches[0].clientX : moveEvent.clientX;
-			const moveY = 'touches' in moveEvent ? moveEvent.touches[0].clientY : moveEvent.clientY;
-
-			let newLeft = moveX - shiftX;
-			let newTop = moveY - shiftY;
-
-			const asideWidth = aside.offsetWidth;
-			const asideHeight = aside.offsetHeight;
-
-			// Limites da tela
-			if (newLeft < 0) newLeft = 0;
-			if (newLeft + asideWidth > window.innerWidth) newLeft = window.innerWidth - asideWidth;
-			if (newTop < 0) newTop = 0;
-			if (newTop + asideHeight > window.innerHeight) newTop = window.innerHeight - asideHeight;
-
-			aside.style.left = `${newLeft}px`;
-			aside.style.top = `${newTop}px`;
-		};
-
-		const onEnd = () => {
-			document.removeEventListener('mousemove', onMove as any);
-			document.removeEventListener('mouseup', onEnd);
-			document.removeEventListener('touchmove', onMove as any);
-			document.removeEventListener('touchend', onEnd);
-		};
-
-		document.addEventListener('mousemove', onMove);
-		document.addEventListener('mouseup', onEnd);
-		document.addEventListener('touchmove', onMove, { passive: false });
-		document.addEventListener('touchend', onEnd);
-	};
-
 	return (
 		<AsideContainer
 			ref={asideRef}
 			$collapsed={collapsed}
-			onMouseDown={handleDragStart}
-			onTouchStart={handleDragStart}
+			onMouseDown={(e) => dragAndDrop(asideRef, e)}
+			onTouchStart={(e) => dragAndDrop(asideRef, e)}
 		>
 			<ToggleButton onClick={() => setCollapsed(!collapsed)} $collapsed={collapsed}>
 				{collapsed ? <SquareArrowOutUpRight /> : <SquareArrowOutDownLeft />}
