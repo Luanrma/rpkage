c14f16da4555539a3c24702a6f7af4184fd21220
Luanrma <luanrma@gmail.com>
2025-04-26 03:37:01 -0300

    layout upkeep

--- src/app/components/Aside/index.tsx
+++ src/app/components/Aside/index.tsx
@@ -67,44 +67,57 @@ export default function Aside({ changeSection }: AsideProps) {
 
     const handleMenuItemClick = (value: string) => changeSection(value)
 
-    const handleDrag = (e: React.MouseEvent) => {
-        const aside = asideRef.current
-        if (!aside) return
-
-        const shiftX = e.clientX - aside.getBoundingClientRect().left
-        const shiftY = e.clientY - aside.getBoundingClientRect().top
-
-        const onMouseMove = (moveEvent: MouseEvent) => {
-            let newLeft = moveEvent.clientX - shiftX
-            let newTop = moveEvent.clientY - shiftY
-
-            const asideWidth = aside.offsetWidth
-            const asideHeight = aside.offsetHeight
-
-            // Limitar o movimento horizontalmente
-            if (newLeft < 0) newLeft = 0
-            if (newLeft + asideWidth > window.innerWidth) newLeft = window.innerWidth - asideWidth
-
-            // Limitar o movimento verticalmente
-            if (newTop < 0) newTop = 0
-            if (newTop + asideHeight > window.innerHeight) newTop = window.innerHeight - asideHeight
-
-            aside.style.left = `${newLeft}px`
-            aside.style.top = `${newTop}px`
-        }
-
-        document.addEventListener('mousemove', onMouseMove)
-
-        document.addEventListener('mouseup', () => {
-            document.removeEventListener('mousemove', onMouseMove)
-        }, { once: true })
-    }
+    const handleDragStart = (e: React.MouseEvent | React.TouchEvent) => {
+        const aside = asideRef.current;
+        if (!aside) return;
+      
+        const isTouchEvent = 'touches' in e;
+        const startX = isTouchEvent ? e.touches[0].clientX : e.clientX;
+        const startY = isTouchEvent ? e.touches[0].clientY : e.clientY;
+      
+        const rect = aside.getBoundingClientRect();
+        const shiftX = startX - rect.left;
+        const shiftY = startY - rect.top;
+      
+        const onMove = (moveEvent: MouseEvent | TouchEvent) => {
+          const moveX = 'touches' in moveEvent ? moveEvent.touches[0].clientX : moveEvent.clientX;
+          const moveY = 'touches' in moveEvent ? moveEvent.touches[0].clientY : moveEvent.clientY;
+      
+          let newLeft = moveX - shiftX;
+          let newTop = moveY - shiftY;
+      
+          const asideWidth = aside.offsetWidth;
+          const asideHeight = aside.offsetHeight;
+      
+          // Limites da tela
+          if (newLeft < 0) newLeft = 0;
+          if (newLeft + asideWidth > window.innerWidth) newLeft = window.innerWidth - asideWidth;
+          if (newTop < 0) newTop = 0;
+          if (newTop + asideHeight > window.innerHeight) newTop = window.innerHeight - asideHeight;
+      
+          aside.style.left = `${newLeft}px`;
+          aside.style.top = `${newTop}px`;
+        };
+      
+        const onEnd = () => {
+          document.removeEventListener('mousemove', onMove as any);
+          document.removeEventListener('mouseup', onEnd);
+          document.removeEventListener('touchmove', onMove as any);
+          document.removeEventListener('touchend', onEnd);
+        };
+      
+        document.addEventListener('mousemove', onMove);
+        document.addEventListener('mouseup', onEnd);
+        document.addEventListener('touchmove', onMove, { passive: false });
+        document.addEventListener('touchend', onEnd);
+      };
 
     return (
         <AsideContainer
             ref={asideRef}
             $collapsed={collapsed}
-            onMouseDown={handleDrag}
+            onMouseDown={handleDragStart}
+            onTouchStart={handleDragStart}
         >
             <ToggleButton onClick={() => setCollapsed(!collapsed)} $collapsed={collapsed}>
                 {collapsed ? <SquareArrowOutUpRight /> : <SquareArrowOutDownLeft />}
