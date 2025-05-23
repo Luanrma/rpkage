f86bd6ea4cc258a80aa4b2c209709157c3e5264a
Anthony Lima <anthonylimac@gmail.com>
2025-05-23 00:39:50 -0300

    implemented  enabled  in navbar

--- src/app/components/Aside/index.tsx
+++ src/app/components/Aside/index.tsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import { Activity, Swords, SquareArrowOutDownLeft, SquareArrowOutUpRight, Backpack, User, House, LogOut, Handshake, SmilePlus } from "lucide-react";
 import LogoutButton from '../LogoutButton';
 import { useSession } from '@/app/contexts/SessionContext';
+import { usePathname } from 'next/navigation'
 
 const AsideContainer = styled.div<{ $collapsed: boolean }>`
   position: block;
@@ -16,7 +17,7 @@ const AsideContainer = styled.div<{ $collapsed: boolean }>`
   align-items: stretch;
   background-color: rgb(49, 49, 49);
   padding: 1rem;
-  width: ${({ $collapsed }) => ($collapsed ? "3.5rem" : "15rem")};
+  width: ${({ $collapsed }) => ($collapsed ? "5rem" : "15rem")};
   height: auto;
   border-radius: 10px;
   transition: width 0.3s ease;
@@ -29,15 +30,19 @@ const MenuList = styled.ul`
   padding: 0;
 `;
 
-const MenuItem = styled.li`
+const MenuItem = styled.li <{ $active?: boolean }>
+	`
   padding: 1rem 0;
-  color: rgb(117, 117, 117);
-  transition: transform 0.2s ease;
+  color: ${({ $active }) => ($active ? 'rgb(255, 255, 255)' : 'rgb(117, 117, 117)')} ;
+  transition: all 0.2s ease;
+	backgroundColor: ${({ $active }) => ($active ? 'white' : 'transparent')}
+	borderradius: 4x;
 
   &:hover {
     color: rgb(255, 255, 255);
     transform: translateX(0.2rem);
   }
+		
 
   a {
     display: flex;
@@ -45,6 +50,11 @@ const MenuItem = styled.li`
     gap: 1rem;
     color: inherit;
     text-decoration: none;
+		padding: 0.5rem;
+  }
+
+	svg {
+    color: ${({ $active }) => ($active ? '#6e3fae' : 'currentColor')};
   }
 `;
 
@@ -52,6 +62,10 @@ const ToggleButton = styled.button<{ $collapsed: boolean }>`
   align-self: ${({ $collapsed }) => ($collapsed ? "center" : "flex-end")};
   margin-bottom: 1rem;
   transition: transform .5s ease;
+	background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
 
   &:hover {
     transform: scale(1.3);
@@ -61,11 +75,14 @@ const ToggleButton = styled.button<{ $collapsed: boolean }>`
 export default function Aside() {
 	const [collapsed, setCollapsed] = useState(true);
 	const { campaignUser } = useSession();
+	const [activeItem, setActiveItem] = useState('/home')
+	const pathname = usePathname(); // Get current route path
+	const [isClient, setIsClient] = useState(false);
 
-	// Novo estado para controlar se está no client
-  	const [isClient, setIsClient] = useState(false);
 
-		// Após montar no client, ativa a renderização real
+
+
+	// Após montar no client, ativa a renderização real
 	useEffect(() => {
 		setIsClient(true);
 	}, []);
@@ -78,6 +95,9 @@ export default function Aside() {
 	const isMaster = campaignUser?.role === 'MASTER';
 	const isPlayer = campaignUser?.role === 'PLAYER';
 
+	const isActive = (path: String) => {
+		return pathname === path;
+	}
 	return (
 		<AsideContainer $collapsed={collapsed}>
 			<ToggleButton onClick={() => setCollapsed(!collapsed)} $collapsed={collapsed}>
@@ -85,32 +105,32 @@ export default function Aside() {
 			</ToggleButton>
 
 			<MenuList>
-				<MenuItem>
+				<MenuItem $active={isActive('/home')}>
 					<Link href="/home"><House />{!collapsed && "Home"}</Link>
 				</MenuItem>
-				<MenuItem>
+				<MenuItem $active={isActive('/character')}>
 					<Link href="/character"><SmilePlus />{!collapsed && "Character"}</Link>
 				</MenuItem>
 				{isMaster && (
-					<MenuItem>
+					<MenuItem $active={isActive('/user')}>
 						<Link href="/user"><User />{!collapsed && "User"}</Link>
 					</MenuItem>
 				)}
-				<MenuItem>
+				<MenuItem $active={isActive('/')}>
 					<Link href="/"><Handshake />{!collapsed && "Campaing"}</Link>
 				</MenuItem>
-			
-					<MenuItem>
-						<Link href="/item-generator"><Swords />{!collapsed && "Item Generator"}</Link>
-					</MenuItem>
-				
-				<MenuItem>
+
+				<MenuItem $active={isActive('/item-generator')}>
+					<Link href="/item-generator"><Swords />{!collapsed && "Item Generator"}</Link>
+				</MenuItem>
+
+				<MenuItem $active={isActive('/damage-calculator')}>
 					<Link href="/damage-calculator"><Activity />{!collapsed && "Damage Calculator"}</Link>
 				</MenuItem>
-				<MenuItem>
+				<MenuItem $active={isActive('/inventory')}>
 					<Link href="/inventory"><Backpack />{!collapsed && "Inventory"}</Link>
 				</MenuItem>
-				<MenuItem>
+				<MenuItem >
 					<LogoutButton><LogOut />{!collapsed && "Logout"}</LogoutButton>
 				</MenuItem>
 			</MenuList>
